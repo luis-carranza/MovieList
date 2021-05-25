524bcc49fdfa3476e03f1757bbd47b600df7bed6
Luis Carranza <carranlu@gmail.com>
2021-05-24 20:38:04 -0500

    Initial Commit

---
+++ MovieList/MovieListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieListApp.swift
+//  MovieList
+//
+//  Created by Luis Javier Carranza on 24/05/21.
+//
+
+import SwiftUI
+
+@main
+struct MovieListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
